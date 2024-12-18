37942b0d161d519e0877544d98f42f878d7fdc94
Benoît Daene <benoitdaene@gmail.com>
2024-12-18 23:25:32 +0100

    More behaviors. Update README.md.

--- README.md
+++ README.md
@@ -11,8 +11,27 @@ Based on https://discordpy.readthedocs.io and https://api.ipify.org/
 - `cd discord-public-ip-publisher`
 - `uv sync`
 
+## Create a bot account
+
+See https://discordpy.readthedocs.io/en/stable/discord.html#discord-intro.
+
+Only permission needed is `text:send_messages`
+
+Enable privileged intent: https://discordpy.readthedocs.io/en/stable/intents.html#privileged-intents
+
+Reset your bot token to allow to copy it (yes reset it).
+Create a file '.discord_token' and copy the token into it.
+
 ## Run
 
 In the project directory (discord-public-ip-publisher)
 
 `uv run discord-public-ip-publisher`
+
+If everything is correctly setup, the bot should say hello on the general channel of the server.
+
+## Usage
+
+Now when typing '\server_ip', the bot should respond with its public IP.
+
+You can also DM (direct message) the bot andit should respond you. 
--- src/discord_public_ip_publisher/discord_bot.py
+++ src/discord_public_ip_publisher/discord_bot.py
@@ -1,20 +1,26 @@
 import logging
 
 import discord
+from discord import DMChannel, TextChannel
 
 from discord_public_ip_publisher.ipify import get_public_ip
 
 _LOGGER = logging.getLogger(__name__)
 
 intents = discord.Intents.default()
-# intents.message_content = True
+intents.message_content = True  # Need "privileged access" ...
 
 client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_ready():
-    logging.info(f'We have logged in as {client.user}')
+    _LOGGER.info(f'We have logged in as {client.user}')
+    public_ip = await get_public_ip()
+    for guild in client.guilds:
+        for channel in guild.channels:
+            if isinstance(channel, TextChannel) and channel.position == 0:
+                await channel.send(f"Hello, my server IP is {public_ip}.")
 
 
 @client.event
@@ -22,11 +28,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    logging.info(f'Received message: "{message.content}" ({message}).')
+    _LOGGER.info(f'Received message: "{message.content}" ({message}).')
+    if message.content.lower().startswith('hello'):
+        await message.channel.send("Hello =)")
+        return
+
     public_ip = await get_public_ip()
     if message.content == '/server_ip':
-        await message.channel.send(public_ip)
-    else:
+        await message.channel.send(f"My public IP is {public_ip}.")
+    elif isinstance(message.channel, DMChannel):
         await message.channel.send(f"I do not know what you mean. But here is my IP: {public_ip}.")
 
 
