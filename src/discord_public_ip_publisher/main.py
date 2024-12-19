2c55da5bfe0516c6c82a7341ba1b625bab6acfa7
Benoît Daene <benoitdaene@gmail.com>
2024-12-19 01:06:03 +0100

    Fix uv run discord-public-ip-publisher

--- src/discord_public_ip_publisher/main.py
+++ src/discord_public_ip_publisher/main.py
@@ -1,13 +1,10 @@
-import asyncio
 import logging
 
 from discord_public_ip_publisher.discord_bot import start_bot
-from discord_public_ip_publisher.ipify import get_public_ip
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    asyncio.run(get_public_ip())
     start_bot()
 
 
